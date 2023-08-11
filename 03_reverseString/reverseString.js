40be24824e40ca89ce16051b6acc1be44d500b2a
Ryan W <28796928+rjwong94@users.noreply.github.com>
2023-08-11 15:56:39 -0600

    Completed javascript 03 reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let result = phrase.split("");
+    result = result.reverse();
+    result = result.join("");
+    result = result.toString();
+    return result
 };
+
+//console.log(reverseString("honey"));
 
 // Do not edit below this line
 module.exports = reverseString;
