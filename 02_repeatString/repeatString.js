5c1c69746bab937edb690c865d6a79cfca463734
Ryan W <28796928+rjwong94@users.noreply.github.com>
2023-08-11 13:38:32 -0600

    Completed first step. Fixed by using sentance = sentance.concat

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,15 @@
 const repeatString = function(word,repeat) {
     let i = 0;
     let sentance = word;
-    while (i < repeat) {
-        sentance.concat(word);
+    while (i < (repeat - 1)) {
+        sentance = sentance.concat(word);
         i++;
     }
 
     return sentance;
 };
 
-console.log(repeatString("Hey", 3));
+//console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
