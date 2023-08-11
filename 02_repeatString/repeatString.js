082f9255499f88621316315f20410ce41bb7a25c
Ryan W <28796928+rjwong94@users.noreply.github.com>
2023-08-11 15:36:45 -0600

    Completed all 02 repeat string exercises successfully

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
 const repeatString = function(word,repeat) {
     let i = 0;
     let sentance = word;
+
+    if (repeat == 0){
+        return "";
+    }
+
+    else if (repeat < 0){
+        return "ERROR";
+    }
+    
     while (i < (repeat - 1)) {
         sentance = sentance.concat(word);
         i++;
