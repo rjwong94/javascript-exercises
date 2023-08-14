496d12c2e68dba053fc547d0fa607119570162ae
Ryan W <28796928+rjwong94@users.noreply.github.com>
2023-08-14 17:23:22 -0600

    completed 04 remove from array assignment. Learned how to copy functions to a new function using the foreach(element). also learned how to compare array to another array by use of the rest parameter and the array.includes() method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...n) {
+    let newArray = [];
+    array.forEach(element => {
+        if (!n.includes(element)) {
+            newArray.push(element);
+        }
+    });
 
+    return newArray;
 };
 
 // Do not edit below this line
