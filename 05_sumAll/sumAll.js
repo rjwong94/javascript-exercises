f01f151ec51fcc92a2060e686641fead43f2952f
Ryan W <28796928+rjwong94@users.noreply.github.com>
2023-08-14 17:54:29 -0600

    completed last 3 problems of o5 sum all assignment to ignore any non integer inputs. This was done by using a multi || if conditional to check if the inputs are either less than 0, or if the number.isinteger() method is false. If any of the conditionals pass, it returns ERROR which completes the last 3 problems. Next I will integrate the actual sum part of the function.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (a < 0 || b < 0 || Number.isInteger(a) == false || Number.isInteger(b) == false){
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
