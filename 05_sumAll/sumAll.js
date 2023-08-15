84d6f3f963f6544d0609252de1be99071dadd909
Ryan W <28796928+rjwong94@users.noreply.github.com>
2023-08-14 18:46:07 -0600

    Completed the remainder of 05 sumall assignment. Solved larger number entered first issue by using conditionals to compare the two numbers and assign them to replacement vars. Used a while statement to compare the 2 new vars where a new sum var adds the counter starting from the lowest number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,28 @@
 const sumAll = function(a,b) {
+    let greaterNum;
+    let lesserNum;
+    let sum = 0;
+    
     if (a < 0 || b < 0 || Number.isInteger(a) == false || Number.isInteger(b) == false){
         return "ERROR";
     }
+    
+    else if(a > b){
+        greaterNum = a;
+        lesserNum = b;
+    }
+    else {
+        greaterNum = b;
+        lesserNum = a;
+    }
+
+    while (lesserNum <= greaterNum) {
+        sum += lesserNum;
+        lesserNum++;
+    }
+
+    return sum;
+
 };
 
 // Do not edit below this line
